59658757d1f5702f720927ace220553a59b928bd
Joshua Lopez <44761768+jflopezr11@users.noreply.github.com>
2020-04-22 22:23:33 -0700

    if two projects

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Joshua Lopez on 4/21/20.
+//  Copyright © 2020 Joshua Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
