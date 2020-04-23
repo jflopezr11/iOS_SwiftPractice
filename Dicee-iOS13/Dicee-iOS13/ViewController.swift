18baa9c0ca275348ba96ededf22b37f2f66b82fc
Joshua Lopez <44761768+jflopezr11@users.noreply.github.com>
2020-04-22 21:57:02 -0700

    new commit

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -15,33 +15,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //who.what= value
-/* diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-     diceImageView1.alpha = 0.5
-       diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
- */
-        
-    }
+
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-      /*  diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
-      */
-        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ] [leftDiceNumber]
-        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ] [rightDiceNumber]
+    
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
         
-        leftDiceNumber = leftDiceNumber + 1
-        
-        rightDiceNumber = rightDiceNumber - 1
-        
-        
-        
-        
+        diceImageView1.image = diceArray.randomElement() //Returns a random element of the collection.
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    
     }
 }
 
