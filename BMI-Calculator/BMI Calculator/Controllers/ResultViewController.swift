96e94d1b217e3417bf14089394e363709c1534e6
Joshua Lopez <44761768+jflopezr11@users.noreply.github.com>
2020-05-04 12:32:02 -0700

    BMI CALCULATOR

---
+++ BMI-Calculator/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Joshua Lopez on 5/1/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
